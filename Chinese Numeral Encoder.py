ebd8c54ac67d63dc23e45cd5d00c9d669d895987
Hector G Barron <hectorgbarron@gmail.com>
2023-04-16 19:37:21 -0500

    pending adding a zero for trailing zeros

--- Chinese Numeral Encoder.py
+++ Chinese Numeral Encoder.py
@@ -1,4 +1,29 @@
-numerals = {
+def to_chinese_numeral(n)->str:
+    print(n)
+    if n==None or n==0:
+        return '零'
+    negativeSymbol=None
+
+    if n<0:
+        negativeSymbol='负'
+        n=n*-1
+            
+    integer, decimal =split(n)
+    integer=integerToNumeral(integer)
+    returnValue=''
+    
+    #Transforming the decimals to Numeral
+    if decimal!=None:
+        decimal= decimalsToNumeral(decimal)
+        returnValue= integer+decimal
+    else:
+        returnValue=integer
+         
+    if negativeSymbol !=None:
+        return negativeSymbol+returnValue
+    return returnValue
+
+numeralsKeys = {
 "-":"负",
 ".":"点",
 0:"零",
@@ -15,47 +40,42 @@ numerals = {
 100:"百",
 1000:"千",
 10000:"万"}
-
-
-def splitFirstAndRemaining(n)->int:
-    n=str(n)
-    return  (n[0],n[1:]) 
-
-def to_chinese_numeral(n)->str:
-    print(n)
-    if n==None or n==0:
-        return '零'
-    if n<0:
-        negativeSymbol='负'
-        n=n*-1
-    else:
-        negativeSymbol=None
-        
-    integer, decimal =split(n)
-    integer=integerToNumeral(integer)
-    returnValue=''
-    
-    #Transforming the decimals to Numeral
-    if decimal!=None:
-        decimal= decimalsToNumeral(decimal)
-        returnValue= str(integer)+decimal
-    else:
-        returnValue=integer
-         
-    if negativeSymbol !=None:
-        return negativeSymbol+returnValue
-    return returnValue
-    
-
-def integerToNumeral(n)->str:    
-   if type(n)==str:
-       n=str(n)
-   return [(val, pos )for pos, val in  enumerate(list( num for num  in n)[::-1],start=1)]   
-      
-
-#[(elem,x) for x, elem in enumerate (list([x for x in test]), start=1)]
-   
+positionKeys={2:10,3:100,4:1000,5:10000}    
  
+def numerals(x, pos=0):
+    if type(x)==str:
+        x=int(x)
+    
+    
+    if pos<=1:
+        return numeralsKeys[x]    
+        
+    return numerals(x)+ numerals(positionKeys[pos])
+        
+def integerToNumeral(n)->str:
+   ''''This method receive a number either as a string type or an int type
+       It generates a list ot tuples breaking down the number an its position
+       **Note the list is in reverse order such as the first digit has the first position***
+   '''     
+   if type(n)==str:
+       n=int(n)
+   if n==0:
+       return numerals(n)
+   
+   if n<=10:
+       return numerals(n,1)
+   if n<=19:
+       
+       return numerals(10,1) + numerals(n-10,1)
+   
+   n=str(n) 
+   numbersBreakDown=[(pos,int(val) )for pos, val in  enumerate(list( num for num  in n)[::-1],start=1)]   
+   listWithoutTrailingZeros= [number for number in numbersBreakDown if not(number[1]==0 and number[0]>0)]
+   returnValue=''
+   for element in listWithoutTrailingZeros[::-1]:
+       returnValue=returnValue+numerals(element[1],element[0])        
+   return returnValue
+
 def split(n)->list():
     n=str(n)
     returnList=n.split('.')
@@ -69,13 +89,14 @@ def decimalsToNumeral(n)->str:
     if type(n)==int:
         n=str(n)   
     dot='点'
-    returnStr=''.join([ numerals[int(char)] for char in n])
+    if len(n)>1:
+        returnStr=''
+        for element in [char for char  in n]:
+            returnStr=returnStr+numerals(element)
+            
+    else:
+        returnStr=numerals(int(n))
     return dot+returnStr
 
 
-print(to_chinese_numeral(-54321))
-
-def testfunct(x):
-    return x+1
-
-integerToNumeral('12345')
+print (to_chinese_numeral(9))
